5bdb9805c25e319f2dc6001ea4f73f4b649ead06
mayank raghuvanshi <mayank1272@hotmail.com>
2020-06-18 01:41:56 +0530

    build added

---
+++ client/build/precache-manifest.499b45d70eebdc6dcf953448fc9d86a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01cdf56b71c8b6ed0fd9365a1aefd3e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05c2feafa844c2624e65",
+    "url": "/static/css/main.8fa36673.chunk.css"
+  },
+  {
+    "revision": "73d5211cc8ed43961719",
+    "url": "/static/js/2.b650d78c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b650d78c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05c2feafa844c2624e65",
+    "url": "/static/js/main.2515646d.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
